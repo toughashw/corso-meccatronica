a4712660074aedba451292c4a3b43c3c9633124b
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2023-04-20 21:17:20 +0000

    Progetto Interruttore Touch che accende un LED

---
+++ Progetti/touch_int_led.ino
@@ -0,0 +1,23 @@
+const int touchint = 7;
+const int led = 12;
+// dichiaro i PINS di Ingressi ed Uscite
+
+void setup () {
+pinMode(touchint, INPUT);
+pinMode(led, OUTPUT);
+Serial.begin(9600);
+}
+// definisco tramite la PinMode chi è INPUT e chi OUTPUT
+
+void loop () {
+boolean statoint = digitalRead(touchint);
+// leggo tramite la digitalRead lo stato dell'interruttore touch
+
+if (statoint == HIGH) {
+    digitalWrite(led, LOW)
+}
+// se è HIGH(1) il fotointerruttore si comporta come un circuito aperto e di conseguenza il led non si accenderà per mancato passaggio di corrente
+else {
+    digitalWrite(led,HIGH)
+}
+// else (altrimenti) lo statoint sarà a LOW e di conseguenza il led si accenderà
