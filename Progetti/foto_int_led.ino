833c436c1eecf54d2010019d8e327be2463e4dce
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2023-04-20 20:55:37 +0000

    Progetto Foto Interruttore che accende un LED

---
+++ Progetti/foto_int_led.ino
@@ -0,0 +1,23 @@
+const int fotointerruttore = 7;
+const int led = 12;
+// dichiaro i PINS di Ingressi ed Uscite
+
+void setup () {
+pinMode(fotointerruttore, INPUT);
+pinMode(led, OUTPUT);
+}
+// definisco tramite la PinMode chi è INPUT e chi OUTPUT
+
+boolean statoint = digitalRead(fotointerruttore);
+// leggo tramite la digitalRead il valore booleano (0/1) del fotointerruttore
+
+void loop () {
+if (statoint == HIGH) {
+    digitalWrite(led, LOW)
+}
+// se è HIGH(1) il fotointerruttore si comporta come un circuito aperto e di conseguenza il led non si accenderà per mancato passaggio di corrente
+else {
+    digitalWrite(led,HIGH)
+}
+// else (altrimenti) lo statoint sarà a LOW e di conseguenza il led si accenderà
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# corso-meccatronica
+# Corso di Meccatronica
 Repository del Corso di Meccatronica della Regione Puglia
