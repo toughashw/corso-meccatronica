5d74cd7b2a1347206b7bccf80f2e7adae5bb35fc
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2023-04-21 08:43:51 +0000

    update projects

--- Progetti/foto_int_led.ino
+++ Progetti/foto_int_led.ino
@@ -10,15 +10,14 @@ pinMode(led, OUTPUT);
 
 void loop () {
 boolean statoint = digitalRead(fotointerruttore);
-Serial.println("Lo stato del foto interruttore è: ",statoint);
 // leggo tramite la digitalRead il valore booleano (0/1) del fotointerruttore
 
 if (statoint == HIGH) {
-    digitalWrite(led, LOW)
+    digitalWrite(led, LOW);
 }
 // se è HIGH(1) il fotointerruttore si comporta come un circuito aperto e di conseguenza il led non si accenderà per mancato passaggio di corrente
 else {
-    digitalWrite(led,HIGH)
+    digitalWrite(led,HIGH);
 }
 // else (altrimenti) lo statoint sarà a LOW e di conseguenza il led si accenderà
 }
--- Progetti/touch_int_led.ino
+++ Progetti/touch_int_led.ino
@@ -5,20 +5,19 @@ const int led = 12;
 void setup () {
 pinMode(touchint, INPUT);
 pinMode(led, OUTPUT);
-Serial.begin(9600);
 }
 // definisco tramite la PinMode chi è INPUT e chi OUTPUT
 
 void loop () {
 boolean statoint = digitalRead(touchint);
-Serial.println("Lo stato dell'interruttore touch è: ",statoint);
 // leggo tramite la digitalRead lo stato dell'interruttore touch
 
 if (statoint == HIGH) {
-    digitalWrite(led, LOW)
+    digitalWrite(led, HIGH);
 }
 // se è HIGH(1) il fotointerruttore si comporta come un circuito aperto e di conseguenza il led non si accenderà per mancato passaggio di corrente
 else {
-    digitalWrite(led,HIGH)
+    digitalWrite(led,LOW);
 }
 // else (altrimenti) lo statoint sarà a LOW e di conseguenza il led si accenderà
+}
